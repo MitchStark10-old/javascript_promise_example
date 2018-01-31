9016d8e45944ec0cb1b29803089764df25977799
Mitch Stark <mitch.stark.github@gmail.com>
2018-01-31 13:15:15 -0500

    add javascript promise example

---
+++ example.js
@@ -0,0 +1,23 @@
+function generateRandomNumber () {
+    return new Promise(function (resolve, reject) {
+      var randomNumber = Math.floor((Math.random() * 10) + 1)
+      if (randomNumber <= 5) {
+        resolve(randomNumber)
+      } else {
+        reject(randomNumber)
+      }
+    })
+  }
+  
+
+async function testResult() {
+    try {
+        let p =  await generateRandomNumber()
+        console.log("Success : " + p);
+    } catch (e) {
+        console.log("Error : " + e);
+    }
+
+}
+  
+testResult()
